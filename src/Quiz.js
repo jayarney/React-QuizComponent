ccc2e38e40faef07ca36cde4b43c4850cae113c5
Jay Arney <jay.arney@sterlingts.com>
2018-06-04 14:19:09 -0700

    Commit

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,6 +9,11 @@ class Quiz extends Component {
     super(props)
     this.state = { quiz_position: 1 }
   }
+  showNextQuestion() {
+    this.setState((state) => {
+      return {quiz_position: state.quiz_position + 1}
+    })
+  }
   render() {
     const isQuizEnd = (this.state.quiz_position - 1 == quizData.quiz_questions.length)
     if (isQuizEnd) {
@@ -17,7 +22,7 @@ class Quiz extends Component {
       )
     }
     return (
-      <div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /></div>
+      <div><QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} /></div>
     )
   }
 }
